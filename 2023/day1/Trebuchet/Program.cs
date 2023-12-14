50003e6865f7bf0bbade2ffc344398a174317004
Koen Kamman <koen@kamman.dev>
2023-12-14 16:16:01 +0100

    Advent of Code 2023 - Day 1 - Part 1

---
+++ 2023/day1/Trebuchet/Program.cs
@@ -0,0 +1,55 @@
+﻿var inputFilePath = GetInputFilePath();
+
+var result = 0;
+foreach (var entry in ReadCalibrationDocument(inputFilePath))
+{
+    result += CalculateCalibrationValue(entry);
+}
+
+Console.WriteLine("Result: " + result);
+
+Console.ReadKey();
+Environment.Exit(0);
+
+static string GetInputFilePath()
+{
+    Console.WriteLine("Calibration document path:");
+    var inputFilePath = Console.ReadLine();
+    if (inputFilePath == null || !File.Exists(inputFilePath))
+    {
+        Console.WriteLine("Invalid input file path.");
+        return GetInputFilePath();
+    }
+    return inputFilePath;
+}
+
+static IEnumerable<string> ReadCalibrationDocument(string filePath)
+{
+    using var streamReader = new StreamReader(filePath);
+    while (!streamReader.EndOfStream)
+    {
+        var line = streamReader.ReadLine();
+        if (line != null)
+        {
+            yield return line;
+        }
+    }
+}
+
+static int CalculateCalibrationValue(string text)
+{
+    var result = string.Empty;
+    char? lastSeenDigit = null;
+    foreach (var character in text)
+    {
+        if (char.IsDigit(character))
+        {
+            if (lastSeenDigit == null)
+            {
+                result += character;
+            }
+            lastSeenDigit = character;
+        }
+    }
+    return int.Parse(result + lastSeenDigit);
+}
