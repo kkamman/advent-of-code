71e6506a1b995427380001bdbf405afe38b579d3
Koen Kamman <koen@kamman.dev>
2023-12-14 19:28:18 +0100

    Advent of Code 2023 - Day 1 - Part 2 WIP

--- 2023/day1/Trebuchet/Program.cs
+++ 2023/day1/Trebuchet/Program.cs
@@ -1,12 +1,17 @@
-﻿var inputFilePath = GetInputFilePath();
+﻿using System.Text;
+
+var inputFilePath = GetInputFilePath();
 
 var result = 0;
+var resultUsingSpelledOutDigits = 0;
 foreach (var entry in ReadCalibrationDocument(inputFilePath))
 {
     result += CalculateCalibrationValue(entry);
+    resultUsingSpelledOutDigits += CalculateCalibrationValue(TransformSpelledOutDigitsToActualDigits(entry));
 }
 
 Console.WriteLine("Result: " + result);
+Console.WriteLine("Result using spelled out digits: " + resultUsingSpelledOutDigits);
 
 Console.ReadKey();
 Environment.Exit(0);
@@ -38,18 +43,37 @@ static IEnumerable<string> ReadCalibrationDocument(string filePath)
 
 static int CalculateCalibrationValue(string text)
 {
-    var result = string.Empty;
-    char? lastSeenDigit = null;
-    foreach (var character in text)
+    var digits = new char[2]
     {
-        if (char.IsDigit(character))
-        {
-            if (lastSeenDigit == null)
-            {
-                result += character;
-            }
-            lastSeenDigit = character;
-        }
+        text.FirstOrDefault(char.IsDigit),
+        text.LastOrDefault(char.IsDigit)
+    };
+
+    return int.TryParse(digits, out int calibrationValue)
+        ? calibrationValue
+        : 0;
+}
+
+// Does not work, spelled out digits overlap.
+static string TransformSpelledOutDigitsToActualDigits(string text)
+{
+    IReadOnlyDictionary<string, string> spelledOutDigitsDictionary = new Dictionary<string, string>()
+    {
+        { "one", "1" },
+        { "two", "2" },
+        { "three", "3" },
+        { "four", "4" },
+        { "five", "5" },
+        { "six", "6" },
+        { "seven", "7" },
+        { "eight", "8" },
+        { "nine", "9" }
+    };
+
+    var stringBuilder = new StringBuilder(text);
+    foreach (var (spelledOutDigit, digit) in spelledOutDigitsDictionary)
+    {
+        stringBuilder.Replace(spelledOutDigit, digit);
     }
-    return int.Parse(result + lastSeenDigit);
+    return stringBuilder.ToString();
 }
